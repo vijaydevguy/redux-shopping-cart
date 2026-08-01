dcd7dde3060afc903a71d283019398d52ab44f07
vijaydevguy <vijayakumarjob01@gmail.com>
2026-08-01 17:44:22 +0530

    tailwind_config

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     base setup
+      <p className="text-[120px] text-red-600">base setup</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
