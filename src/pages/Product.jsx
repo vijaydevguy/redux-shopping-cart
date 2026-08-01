56c1f2a23da8562104d5809ec34cbbb32063ff35
vijaydevguy <vijayakumarjob01@gmail.com>
2026-08-01 19:46:20 +0530

    basic_pages

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { data } from "../data";
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
-}
+  const { id } = useParams();
 
-export default Product
+  const product = data.filter((f) => f.url == id);
+  // console.log(product);
+
+  return (
+    <div className="commonPadding grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-5">
+      {product[0].menu.map((item, i) => (
+        <div key={i}>
+          <img
+            src={item.image}
+            alt="restaurant"
+            className="w-full h-full max-h-[250px] object-center object-cover select-none pointer-events-none rounded-[12px] overflow-hidden "
+          />
+          <h2>{item.name}</h2>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Product;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,15 +6,18 @@ import { slugify } from "../utils/sligify";
 
 const Products = () => {
   return (
-    <div className="commonPadding grid grid-cols-4 gap-5">
+    <div className="commonPadding grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-5">
       {data.map((res, i) => (
         <Link
-          to={`${slugify(res.name)}`}
+          to={`${slugify(res.url)}`}
           className="relative flex flex-col gap-3 "
           key={i}
         >
           <img
-            src={res.image}
+            src={
+              res.image ||
+              "https://images.unsplash.com/photo-1631452180519-c014fe946bc7?w=300&h=200&fit=crop"
+            }
             alt="restaurant"
             className="w-full h-full max-h-[250px] object-center object-cover select-none pointer-events-none rounded-[12px] overflow-hidden "
           />
