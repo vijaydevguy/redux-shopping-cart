2b76e5d7156931190992dabb91839176b9ff0f79
vijaydevguy <vijayakumarjob01@gmail.com>
2026-08-07 23:10:01 +0530

    ui_fix

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,9 @@
 import React, { useReducer } from "react";
 import { useParams } from "react-router-dom";
 import { data } from "../data";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../redux/slices/cartSlice";
+import { cartItems } from "../redux/selectors/cartSelectors";
 
 const Product = () => {
   const { id } = useParams();
@@ -11,31 +12,36 @@ const Product = () => {
   // console.log(product);
 
   const dispatch = useDispatch();
+  const itemsInCart = useSelector(cartItems);
   // console.log(id, "testResID");
 
   return (
     <div className="commonPadding grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-5 gap-y-5">
-      {product[0].menu.map((item, i) => (
-        <div key={i} className="flex flex-col gap-2">
-          <img
-            src={item.image}
-            alt="restaurant"
-            className="w-full h-full max-h-[250px] object-center object-cover select-none pointer-events-none rounded-[12px] overflow-hidden "
-          />
+      {product[0].menu.map((item, i) => {
+        const isAdded = itemsInCart.some((cartItem) => cartItem.id === item.id);
+        return (
+          <div key={i} className="flex flex-col gap-2">
+            <img
+              src={item.image}
+              alt="restaurant"
+              className="w-full h-full max-h-[250px] object-center object-cover select-none pointer-events-none rounded-[12px] overflow-hidden "
+            />
 
-          <div className="flex justify-between gap-2 items-center">
-            <h2>{item.name}</h2>
-            <button
-              className="cursor-pointer underline"
-              onClick={() =>
-                dispatch(addToCart({ restaurantId: id, item: item }))
-              }
-            >
-              Add to Cart
-            </button>
+            <div className="flex justify-between gap-2 items-center">
+              <h2>{item.name}</h2>
+              <button
+                className={`cursor-pointer underline ${isAdded ? "text-gray-500 no-underline cursor-not-allowed" : ""}`}
+                disabled={isAdded}
+                onClick={() =>
+                  dispatch(addToCart({ restaurantId: id, item: item }))
+                }
+              >
+                {isAdded ? "Added" : "Add"}
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
